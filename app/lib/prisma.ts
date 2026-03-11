be6d5b8c53a98a174d4b0d7204dc892d766fcd2e
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2026-03-11 21:47:25 +0800

    fix prisma client

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
+
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "../generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
